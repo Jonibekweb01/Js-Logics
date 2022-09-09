beb01a4b29a7d7093958d71a936f0058650f17a8
Jonibekweb01 <jonibekqodirovv@gmail.com>
2022-09-09 21:13:05 +0500

    Add

---
+++ js/main.js
@@ -0,0 +1,56 @@
+// Masala - 1
+
+function addition(a, b) {
+  return a + b;
+}
+
+// Masala - 2
+
+function convert(minutes) {
+  return Math.floor(minutes * 60);
+}
+
+// Masala - 3
+
+function addition(num) {
+  return num + 1;
+}
+
+// Masala - 4
+
+function triArea(base, height) {
+  return (base * height) / 2;
+}
+
+// Masala - 5
+
+function calcAge(age) {
+  return age * 365;
+}
+
+// Masala - 6
+
+// ?
+
+// Masala - 7
+
+function footballPoints(wins, draws, losses) {
+  return wins * 3 + draws * 1 + losses * 0;
+}
+
+// Masala - 8
+
+function points(twoPointers, threePointers) {
+  return twoPointers * 2 + threePointers * 3;
+}
+
+// MAsala - 9
+
+function lessThan100(a, b) {
+  let check = a + b;
+  if (check < 100) {
+    return true;
+  } else {
+    return false;
+  }
+}
